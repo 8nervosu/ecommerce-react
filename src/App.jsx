6a1b578a562f05bf4ca0106e32f53c776d9192a1
Igor Azevedo <igor.azevedo19@gmail.com>
2024-12-24 19:01:56 -0300

    Atualização

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Header from './components/menu/NavBar';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './components/saudacao/theme';
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div> Olá
+    <><>
+      <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -28,8 +31,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </><ThemeProvider theme={theme}>
+        <Header />
+      </ThemeProvider></>
   )
+  
 }
 
 export default App
